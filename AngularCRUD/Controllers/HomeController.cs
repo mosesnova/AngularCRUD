661247a2843a5d564020ae0354543bc4063641c4
Moses Nova <mosesabhisheknova@yahoo.com>
2018-01-19 12:31:10 +1300

    Add project files.

---
+++ AngularCRUD/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using AngularCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Insert_Employee(Employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    Obj.Employees.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+        public string Update_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    Employee EmpObj = Obj.Employees.Where(x => x.Emp_Id == Emp.Emp_Id).FirstOrDefault();
+                    EmpObj.Emp_Age = Emp.Emp_Age;
+                    EmpObj.Emp_City = Emp.Emp_City;
+                    EmpObj.Emp_Name = Emp.Emp_Name;
+                    Obj.SaveChanges();
+                    return "Employee Updated Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Updated! Try Again";
+            }
+        }
+        public string Delete_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeEntities Obj = new EmployeeEntities())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Employees.Attach(Emp);
+                        Obj.Employees.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }
+        }
+        public JsonResult Get_AllEmployee()
+        {
+            using (EmployeeEntities Obj = new EmployeeEntities())
+            {
+                List<Employee> Emp = Obj.Employees.ToList();
+                return Json(Emp, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
